305a5a8302b161ececd5bd1fada6d8b867aa128d
Hemanth Malla <hemanthmalla@gmail.com>
2019-01-26 15:35:37 -0800

    Initial commit

---
+++ src/test/java/systems/designing/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package systems.designing;
+
+import org.junit.Test;
+import systems.designing.raft.HelloWorld;
+
+public class HelloWorldTest {
+
+    @Test
+    public void main() {
+        HelloWorld.main(new String[]{});
+    }
+}
